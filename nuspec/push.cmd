86ac0c7052d2aeb0c92ebc4fb2bc6a90325ae29d
Allan Ritchie <allan.ritchie@gmail.com>
2017-02-05 14:21:22 -0500

    minor fixes - still broken - waiting for tool fixes

--- nuspec/push.cmd
+++ nuspec/push.cmd
@@ -1,4 +1,4 @@
 @echo off
 copy *.nupkg %HOMEPATH%\dropbox\nuget\ /y
-nuget push *.nupkg -Source https://www.nuget.org/api/v2/package
+rem nuget push *.nupkg -Source https://www.nuget.org/api/v2/package
 pause
