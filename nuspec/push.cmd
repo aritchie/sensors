df17237b336c1c0e754b2a200761c498bab8d946
Allan Ritchie <allan.ritchie@gmail.com>
2017-02-14 21:02:06 -0500

    update push

--- nuspec/push.cmd
+++ nuspec/push.cmd
@@ -1,4 +1,4 @@
 @echo off
 copy *.nupkg %HOMEPATH%\dropbox\nuget\ /y
-rem nuget push *.nupkg -Source https://www.nuget.org/api/v2/package
+nuget push *.nupkg -Source https://www.nuget.org/api/v2/package
 pause
