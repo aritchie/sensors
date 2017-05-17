2103822d2cc122a2e69880c7d150c4c115554b07
Allan Ritchie <allan.ritchie@gmail.com>
2017-05-17 13:33:32 -0400

    cardinal direction calculation for compass reading

---
+++ Plugin.Sensors.Abstractions/CardinalDirection.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace Plugin.Sensors
+{
+    public enum CardinalDirection
+    {
+        North = 0,
+        NorthEast = 1,
+        East = 2,
+        SouthEast = 3,
+        South = 4,
+        SouthWest = 5,
+        West = 6,
+        NorthWest = 7
+    }
+}
