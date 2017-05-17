2d2b6d78d8a4efac8032d23aabca7cc911f2ce5f
Allan Ritchie <allan.ritchie@gmail.com>
2017-05-17 13:33:45 -0400

    compass implementation on android

--- Plugin.Sensors.Android/CompassImpl.cs
+++ Plugin.Sensors.Android/CompassImpl.cs
@@ -1,31 +1,92 @@
 using System;
+using System.Linq;
 using System.Reactive.Linq;
 using Android.App;
 using Android.Content;
 using Android.Hardware;
+using Math = Java.Lang.Math;
 
 
 namespace Plugin.Sensors
 {
     public class CompassImpl : ICompass
     {
+        readonly object syncLock;
         readonly SensorManager sensorManager;
+        readonly CompassReading reading;
+        readonly float[] rMatrix = new float[9];
+        float[] orientation;
+        float[] lastAccel;
+        float[] lastMag;
 
 
         public CompassImpl()
         {
+            this.syncLock = new object();
             this.sensorManager = (SensorManager)Application.Context.GetSystemService(Context.SensorService);
-            //var mgr = new AcrSensorManager(this.sensorManager);
-            //var delay = this.ToSensorDelay(this.ReportInterval);
         }
 
 
-        public bool IsAvailable => false;
+        public bool IsAvailable => this.sensorManager.GetDefaultSensor(SensorType.Accelerometer) != null &&
+                                   this.sensorManager.GetDefaultSensor(SensorType.MagneticField) != null;
 
 
+        IObservable<CompassReading> readOb;
         public IObservable<CompassReading> WhenReadingTaken()
         {
-            return Observable.Empty<CompassReading>();
+            if (!this.IsAvailable)
+                return Observable.Empty<CompassReading>();
+
+            this.readOb = this.readOb ?? Observable.Create<CompassReading>(ob =>
+            {
+                var accelMgr = new AcrSensorManager(this.sensorManager);
+                var magMgr = new AcrSensorManager(this.sensorManager);
+                accelMgr.Start(SensorType.Accelerometer, SensorDelay.Fastest, e =>
+                {
+                    lock (this.syncLock)
+                    {
+                        this.lastAccel = e.Values.ToArray();
+                        this.Calc(ob);
+                    }
+                });
+                magMgr.Start(SensorType.MagneticField, SensorDelay.Fastest, e =>
+                {
+                    lock (this.syncLock)
+                    {
+                        this.lastMag = e.Values.ToArray();
+                        this.Calc(ob);
+                    }
+                });
+                return () =>
+                {
+                    accelMgr.Stop();
+                    magMgr.Stop();
+                };
+            })
+            .Publish()
+            .RefCount();
+
+            return this.readOb;
+        }
+
+
+        void Calc(IObserver<CompassReading> ob)
+        {
+            if (this.lastMag == null || this.lastAccel == null)
+                return;
+
+            SensorManager.GetRotationMatrix(this.rMatrix, null, this.lastAccel, this.lastMag);
+            SensorManager.GetOrientation(this.rMatrix, this.orientation);
+            var degrees = (Math.ToDegrees(this.orientation[0]) + 360) % 360;
+
+            // TODO: not so allocatey
+            // TODO: get compass accuracy
+            // TODO: calculate true north
+            ob.OnNext(new CompassReading(CompassAccuracy.Approximate, degrees, 0));
+
+            // clear for fresh read
+            this.lastMag = null;
+            this.lastAccel = null;
         }
     }
 }
